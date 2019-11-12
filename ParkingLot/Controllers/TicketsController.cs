afb9ddd2e24890540d86f1764d04da08b54e1963
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2019-11-12 17:53:54 +0100

    Remove autogenerated weather code example

---
+++ ParkingLot/Controllers/TicketsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingLot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+
+        public TicketsController(ILogger<TicketsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
